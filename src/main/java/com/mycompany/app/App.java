2afbce0d61378445a49f85ea78b51e3bc0ffb3ea
Karol Harezlak <76980800+karolh2000@users.noreply.github.com>
2023-07-17 13:42:27 -0600

    Docker added

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,23 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
-
+    public static final String MESSAGE = "Hello World!";
+    private String message = "";
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        final App app = new App();
+        for (int i=0; i < 1000; i++) {
+            app.setMessage(MESSAGE + " " + i);
+            System.out.println(MESSAGE + " " + i);
+        }
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     public String getMessage() {
-        return MESSAGE;
+        return this.message;
     }
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -13,6 +13,8 @@ public class AppTest
     public void testAppConstructor() {
         App app1 = new App();
         App app2 = new App();
+        app1.setMessage("Hello World!");
+        app2.setMessage("Hello World!");
         assertEquals(app1.getMessage(), app2.getMessage());
     }
 
@@ -20,6 +22,17 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
+        app.setMessage("Hello World!");
         assertEquals("Hello World!", app.getMessage());
+    }
+
+    @Test
+    public void testCountMessages() {
+        final App app = new App();
+        for (int i=0; i < 1000; i++) {
+            app.setMessage(App.MESSAGE + " " + i);
+            System.out.println(App.MESSAGE + " " + i);
+        }
+        assertEquals(App.MESSAGE + " " + 999, app.getMessage());
     }
 }
